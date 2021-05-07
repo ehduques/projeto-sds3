2992aaf35768ca7195055b793f6c27eb2f569a6a
Eduardo Duques <eduardo_gdsw@outlook.com>
2021-05-07 13:56:44 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import DataTable from "components/DataTable";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/ehduques" target="_blank" rel="noreferrer">Eduardo Duques</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/eh_duques.ig" target="_blank" rel="noreferrer">@eh_duques.ig</a></small></p>
+            </div>
+        </footer>
+    );
+    
+}
+
+export default Footer;
